3d4350615d5cc0bd2bf319fa931c1921c1cd60b8
kvnbanunu <kvnbanunu@gmail.com>
2024-10-01 13:03:47 -0700

    Custom Handlers

---
+++ internal/server/routes.go
@@ -0,0 +1,11 @@
+package server
+
+import "net/http"
+
+
+
+func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(http.StatusText(http.StatusOK)))
+}
--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+    "github.com/kvnbanunu/chirpy/internal/server"
 )
 
 
 
 func main() {
     const port = 8080
+    const filePathRoot = "."
 
     mux := http.NewServeMux()
+    
+    fsHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
+    mux.Handle("/app/", fsHandler)
+    mux.HandleFunc("GET /healthz", server.ReadinessHandler)
+
     server := &http.Server{
         Addr: fmt.Sprintf(":%d",port),
         Handler: mux,
     }
+
     log.Printf("Server listening on port: %d", port)
     log.Fatal(server.ListenAndServe())
 }
